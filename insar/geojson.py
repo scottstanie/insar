69d7470f88c9a5e15ebd8ef03c175b6ff1597dde
scott <scott.stanie@gmail.com>
2018-05-26 15:00:31 -0500

    fix geojson, allow deeper objects

--- insar/geojson.py
+++ insar/geojson.py
@@ -7,15 +7,27 @@ Output: left, bottom, right, top (floats)
 
 import sys
 import json
+from insar.log import get_log
+
+logger = get_log()
 
 
 def geojson_to_bounds(geojson):
+    """From a geojson object, compute bounding lon/lats
+
+    Valid geojson types: Polygon (necessary at some depth), Feature, FeatureCollection
+    """
+    # First, if given a deeper object (e.g. from geojson.io), extract just polygon
     try:
-        assert geojson['type'] == 'Polygon', 'Must use polygon geojson'
-    except KeyError as exc:
+        if geojson.get('type') == 'FeatureCollection':
+            geojson = geojson['features'][0]['geometry']
+        elif geojson.get('type') == 'Feature':
+            geojson = geojson['geometry']
+    except KeyError:
         print("Invalid geojson")
         raise
 
+    assert geojson['type'] == 'Polygon', 'Must use polygon geojson'
     # Note: we are assuming a simple polygon with no holes
     coordinates = geojson['coordinates'][0]
 
@@ -25,18 +37,6 @@ def geojson_to_bounds(geojson):
     top = max(float(lat) for (lon, lat) in coordinates)
     bottom = min(float(lat) for (lon, lat) in coordinates)
     return left, bottom, right, top
-
-
-def srtm_download():
-    # website given links:
-    # http://e4ftl01.cr.usgs.gov//MODV6_Dal_D/SRTM/SRTMGL1N.003/2000.02.11/N19W156.SRTMGL1N.num.zip
-    # http://e4ftl01.cr.usgs.gov//MODV6_Dal_D/SRTM/SRTMGL1N.003/2000.02.11/N18W156.SRTMGL1N.num.zip
-
-    # Elevation grabbed:
-    # https://dds.cr.usgs.gov/srtm/version2_1/Documentation/SRTM_Topo.pdf
-    # https://github.com/bopen/elevation/blob/master/elevation/datasource.py
-    # curl -s -o spool/N20/N20W155.hgt.gz.temp https://s3.amazonaws.com/elevation-tiles-prod/skadi/N20/N20W155.hgt.gz && mv spool/N20/N20W155.hgt.gz.temp spool/N20/N20W155.hgt.gz
-    pass
 
 
 if __name__ == '__main__':
