d0cf4fddf62a17344b4f327f257f705c9ce5cdfb
scott <scott.stanie@gmail.com>
2018-06-08 10:54:48 -0500

    make length an arg for snaphu

--- scripts/run_snaphu.sh
+++ scripts/run_snaphu.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/bash
 
-for INTFILE in `ls -1 *.int`; do 
-	CORNAME=$(echo $INTFILE | sed 's/.int/.cc/' ) 
-	OUTNAME=$(echo $INTFILE | sed 's/.int/.unw/' ) 
+SCRIPTNAME=`basename "$0"`
+# aka length aka xsize aka number of samples in one range line
+LENGTH=$1
+if [ -z $LENGTH ]; then
+	echo "Usage: $SCRIPTNAME LENGTH"
+	exit 1
+fi
+
+
+for INTFILE in `ls -1 *.int`; do
+	CORNAME=$(echo $INTFILE | sed 's/.int/.cc/' )
+	OUTNAME=$(echo $INTFILE | sed 's/.int/.unw/' )
 	echo "Running snaphu on $INTFILE: output to $OUTNAME"
-	~/phase_upwrap/bin/snaphu -s $INTFILE 1000 -c $CORNAME -o $OUTNAME; 
+	~/phase_upwrap/bin/snaphu -s $INTFILE $LENGTH -c $CORNAME -o $OUTNAME;
 done
 
