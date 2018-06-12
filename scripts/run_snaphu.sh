6b5ebf7d16f1099a7314a0d744f13f1a64dbbecd
scott <scott.stanie@gmail.com>
2018-06-12 16:10:06 -0500

    fix variable name for run snaphu

--- scripts/run_snaphu.sh
+++ scripts/run_snaphu.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
 SCRIPTNAME=`basename "$0"`
-# aka length aka xsize aka number of samples in one range line
-LENGTH=$1
-if [ -z $LENGTH ]; then
-	echo "Usage: $SCRIPTNAME LENGTH"
+# aka width aka xsize aka number of samples in one range line
+LINELENGTH=$1
+if [ -z $LINELENGTH ]; then
+	echo "Usage: $SCRIPTNAME LINELENGTH"
 	exit 1
 fi
 
@@ -13,6 +13,6 @@ for INTFILE in `ls -1 *.int`; do
 	CORNAME=$(echo $INTFILE | sed 's/.int/.cc/' )
 	OUTNAME=$(echo $INTFILE | sed 's/.int/.unw/' )
 	echo "Running snaphu on $INTFILE: output to $OUTNAME"
-	~/phase_upwrap/bin/snaphu -s $INTFILE $LENGTH -c $CORNAME -o $OUTNAME;
+	~/phase_upwrap/bin/snaphu -s $INTFILE $LINELENGTH -c $CORNAME -o $OUTNAME;
 done
 
