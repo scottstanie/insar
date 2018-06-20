e719fe009bf0590907bf3eacc20621af3e89bb20
scott <scott.stanie@gmail.com>
2018-06-20 15:27:23 -0500

    start making dem tests

--- insar/tests/test_dem.py
+++ insar/tests/test_dem.py
@@ -1,6 +1,8 @@
 import unittest
+import json
 import tempfile
 import shutil
+from os.path import join, dirname
 import os
 import responses
 
@@ -15,10 +17,26 @@ from insar import geojson
 class TestStartLatLon(unittest.TestCase):
     def setUp(self):
         self.datapath = join(dirname(__file__), 'data')
-        self.geojson = 
-        self.southeast = 'S19E156.hgt'
-        self.bad_tilename = 'somethingelse.hgt'
+        self.geojson_path = join(self.datapath, 'hawaii_small.geojson')
+        with open(self.geojson_path, 'r') as f:
+            self.geojson = json.load(f)
+        self.bounds = geojson.bounding_box(self.geojson)
+        self.download_url = "http://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11/N19W156.SRTMGL1.hgt.zip"
 
     @responses.activate
     def test_s(self):
         responses.add(responses.GET, self.download_url, body=self.sample_hgt, status=200)
+
+
+class TestTile(unittest.TestCase):
+    def setUp(self):
+        self.datapath = join(dirname(__file__), 'data')
+        self.geojson_path = join(self.datapath, 'hawaii_small.geojson')
+        with open(self.geojson_path, 'r') as f:
+            self.geojson = json.load(f)
+        self.bounds = geojson.bounding_box(self.geojson)
+
+    def test_init(self):
+        t = dem.Tile(*self.bounds)
+        expected = (-155.63232421875, 19.4303341116379, -155.3192138671875, 19.730512997022263)
+        self.assertEqual(expected, t.bounds)
