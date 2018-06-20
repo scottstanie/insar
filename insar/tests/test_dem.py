0de6b2d43c7cf997123cfa7bb04fb4863e837e35
scott <scott.stanie@gmail.com>
2018-06-20 17:21:52 -0500

    make download dem test work

--- insar/tests/test_dem.py
+++ insar/tests/test_dem.py
@@ -5,9 +5,10 @@ import shutil
 from os.path import join, dirname
 import os
 import responses
+import numpy as np
+from numpy.testing import assert_array_almost_equal
 
-from insar import dem
-from insar import geojson
+from insar import dem, geojson, sario
 
 DATAPATH = join(dirname(__file__), 'data')
 NETRC_PATH = join(DATAPATH, 'netrc')
@@ -41,12 +42,12 @@ class TestDownload(unittest.TestCase):
         with open(self.geojson_path, 'r') as f:
             self.geojson = json.load(f)
         self.bounds = geojson.bounding_box(self.geojson)
-        self.test_tile = 'N19W156'
+        self.test_tile = 'N19W156.hgt'
         self.hgt_url = "http://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11/N19W156.SRTMGL1.hgt.zip"
 
-        sample_hgt_path = join(DATAPATH, 'N19W156.hgt')
+        sample_hgt_path = join(DATAPATH, self.test_tile + '.zip')
         with open(sample_hgt_path, 'rb') as f:
-            self.sample_hgt = f.read()
+            self.sample_hgt_zip = f.read()
 
         self.cache_dir = tempfile.mkdtemp()
 
@@ -56,5 +57,30 @@ class TestDownload(unittest.TestCase):
         self.assertEqual(d.data_url, "http://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11")
 
     @responses.activate
-    def test_s(self):
-        responses.add(responses.GET, self.hgt_url, body=self.sample_hgt, status=200)
+    def test_download(self):
+        responses.add(responses.GET, self.hgt_url, body=self.sample_hgt_zip, status=200)
+        d = dem.Downloader(
+            [self.test_tile], netrc_file=NETRC_PATH, parallel_ok=False, cache_dir=self.cache_dir)
+        d.download_all()
+        self.assertTrue(os.path.exists(join(d.cache_dir, self.test_tile)))
+
+    def tearDown(self):
+        shutil.rmtree(self.cache_dir)
+
+
+"""
+TODO:
+    finish cropped, upsampled dem, show  this:
+        expected_dem = np.array(
+            [[2071, 2072, 2074, 2076, 2078, 2078, 2079, 2080, 2082], [
+                2071, 2072, 2073, 2075, 2076, 2077, 2078, 2079, 2081
+            ], [2071, 2072, 2073, 2074, 2075, 2076, 2078, 2079, 2080], [
+                2071, 2071, 2072, 2073, 2074, 2075, 2077, 2078, 2080
+            ], [2071, 2071, 2072, 2073, 2074, 2075, 2076, 2078, 2080], [
+                2071, 2071, 2072, 2072, 2073, 2074, 2076, 2077, 2079
+            ], [2071, 2071, 2072, 2072, 2073, 2074, 2076, 2077, 2078]],
+            dtype='<i2')
+
+        output_dem = sario.load_file('elevation.dem')
+        # assert_array_almost_equal(expected_dem)
+    """
