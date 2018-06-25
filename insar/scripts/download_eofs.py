675974f0cee466032674f5c03b13fda07f5ed69f
scott <scott.stanie@gmail.com>
2018-06-25 14:33:03 -0500

    convert main download to cli

--- insar/scripts/download_eofs.py
+++ insar/scripts/download_eofs.py
@@ -11,24 +11,11 @@ logger = get_log()
 
 
 @log_runtime
-def main():
-    parser = argparse.ArgumentParser(
-        description='Download EOFs for specific date, or for Sentinel files in --path. '
-        'With arguments, searches current directory for Sentinel 1 products')
-    parser.add_argument("--date", "-r", help="Validity date for EOF to download")
-    parser.add_argument(
-        "--path", "-p", help="Which directory to look for Sentinel products.", default=".")
-    parser.add_argument(
-        "--mission",
-        "-m",
-        choices=("S1A", "S1B"),
-        help="Specify mission satellite to download (None downloads both S1A and S1B)")
-    args = parser.parse_args()
-
-    if (args.mission and not args.date):
+def main(mission=None, date=None):
+    if (mission and not date):
         logger.error("Must specify date if specifying mission.")
         sys.exit(1)
-    if not args.date:
+    if not date:
         # No command line args given: search current directory
         orbit_dates, missions = insar.eof.find_sentinel_products(args.path)
         if not orbit_dates:
