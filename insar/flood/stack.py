3683420cc558d98d2d79fb63b536027f76bf4a15
scott <scott.stanie@gmail.com>
2018-08-02 11:10:05 -0500

    make box zomoing plot function

--- insar/flood/stack.py
+++ insar/flood/stack.py
@@ -60,9 +60,13 @@ def plot_uavsar_time_diffs(image_list):
 def overlay(under_image, over_image, under_image_info=None, ax=None, alpha=0.5):
     """Plots two images, one under, one transparent over
 
-    under_image will be cmap=gray, over_image with
-    a shifted cmap (seismic default)
+    under_image will be cmap=gray, over_image with shifted cmap, default=seismic
     Assumes images are aligned at top left corner
+
+    Returns:
+        AxesSubplot: axes of the overlaid image
+        under_extend (tuple): tuple used for extent keyword for under_image
+            This is None if under_image_info give
     """
     if not ax:
         fig, ax = plt.subplots(1, 1)
@@ -77,10 +81,46 @@ def overlay(under_image, over_image, under_image_info=None, ax=None, alpha=0.5):
     else:
         # No lat/lon provided: jsut use row, col, no extend arg
         xlabel, ylabel = 'col number', 'row number'
-        under_extent = over_extent = None
+        nrows, ncols = under_image.shape
+        under_extent = (0, ncols, nrows, 0)
+        nrows, ncols = over_image.shape
+        over_extent = (0, ncols, nrows, 0)
+
     ax.imshow(under_image, cmap='gray', extent=under_extent)
     ax.set_xlabel(xlabel)
     ax.set_ylabel(ylabel)
     cmap = make_shifted_cmap(over_image, cmap_name='seismic')
     ax.imshow(over_image, cmap=cmap, alpha=0.5, extent=over_extent)
-    return ax
+    return ax, over_extent
+
+
+def zoomed_box(image, bbox, zoom=500, loc='upper right'):
+    """
+    Args:
+        bbox (tuple): (left, bottom, width, height)
+    """
+    x0, y0 = 500, 1000
+    axins = zoomed_inset_axes(ax, zoom, loc=loc)
+
+    # TODO: how to recreate this with multiple imshow steps??
+    # Ideally pass in the axins
+    ax, extent = stack.overlay(under, rlist[0])
+    axins = zoomed_inset_axes(ax, zoom, loc='upper right')
+
+    axins.imshow(image)
+    # axins.imshow(under, cmap='gray')
+    # axins.imshow(rlist[0], cmap=cmap, alpha=0.5)
+
+    x0, y0, width, height
+    axins.set_xlim(x0, x0 + width)
+    axins.set_ylim(y0 + height, y0)
+
+    # TODO: making this pick locations based on 'loc'?
+    # fc = facecolor, ec = edgecolor
+    _patch, pp1, pp2 = mark_inset(ax, axins, loc1=3, loc2=1, fc="none", ec="0.5")
+    # For inverted axes (in image), must flip manually
+    # https://stackoverflow.com/a/48987903/4174466
+    pp1.loc1, pp1.loc2 = 2, 3
+    pp2.loc1, pp2.loc2 = 4, 1
+    plt.draw()
+    return axins
