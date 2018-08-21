23f1e7530bc4e5894a4744f6fa15f695ef3abb2e
scott <scott.stanie@gmail.com>
2018-08-21 09:49:42 -0500

    more moveing to separate repos for dem

--- insar/__init__.py
+++ insar/__init__.py
@@ -3,7 +3,6 @@ warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 from . import dem
 from . import eof
-from . import geojson
 from . import log
 from . import parsers
 from . import plotting
