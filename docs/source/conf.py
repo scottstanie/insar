027b2f664b1244de6a824c66f70b1a7ddef6dfa0
scott <scott.stanie@gmail.com>
2018-09-02 18:25:36 -0500

    idk rtfd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,8 @@
 #
 import os
 import sys
+sys.path.insert(0, os.path.abspath('../insar'))
+sys.path.insert(0, os.path.abspath('../insar/insar'))
 sys.path.insert(0, os.path.abspath('../../insar'))
 sys.path.insert(0, os.path.abspath('../../insar/insar'))
 
