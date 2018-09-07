68b6b940c2cba4b6028c7ff991e9db74c9ffced8
scott <scott.stanie@gmail.com>
2018-09-07 09:32:02 -0500

    refactor tile funcs into TileGrid

--- insar/tile.py
+++ insar/tile.py
@@ -75,115 +75,131 @@ class Tile(object):
         return insar.latlon.intersects(self.extent, extent)
 
 
-def total_swath_extent(sentinel_list):
-    """Take a list of Sentinels, and find total extent of area covered
-
-    Args:
-        list[Sentinel]: Sentinel objects covering one area
-
-    Returns:
-        tuple[float]: (min_lon, max_lon, min_lat, max_lat)
+class TileGrid(object):
     """
-    swath_extents = np.array([s.swath_extent for s in sentinel_list])
-    min_lon, _, min_lat, _ = np.min(swath_extents, axis=0)
-    _, max_lon, _, max_lat = np.max(swath_extents, axis=0)
-    return min_lon, max_lon, min_lat, max_lat
+    Class handling making tiles out of list of Sentinel objects
 
-
-def total_swath_width_height(sentinel_list):
-    """Width and height in deg across all swath areas in sentinel_list
-
-    Width is longitude extent, height is latitude extent
-
-    Args:
-        list[Sentinel]: Sentinel objects covering one area
-
-    Returns:
-        tuple[float]: (size_lon, size_lat) in degrees
-    """
-    left, right, bot, top = total_swath_extent(sentinel_list)
-    return right - left, top - bot
-
-
-def num_tiles(length, tile_size=0.5, overlap=0.1):
-    """Given an edge length (of a swath), find number of tiles in a direction
-
-    Illustration: Dividing 1.3 total length into 3 overlap blocks
-    |     | |   | |    |
-    -----------------------
-      .4  .1 .3 .1  .4
-
-    Examples:
-    >>> num_tiles(1.3, tile_size=0.5, overlap=0.1)
-    3
-    >>> num_tiles(np.array([1.3, 1.3]), tile_size=0.5, overlap=0.1)
-    array([3, 3])
-    >>> num_tiles(np.array([1.4, 1.5, 1.6]), tile_size=0.5, overlap=0.1)
-    array([3, 3, 4])
-    """
-    covered_width = tile_size - overlap
-    length_to_divide = length - overlap
-    return np.round((length_to_divide) / covered_width).astype(int)
-
-
-def tile_dims(swath_length_width, tile_size=0.5, overlap=0.1):
-    """Given a swath (size_lon, width), find sizes of tiles
-
-    Will try to divide into about 0.5 degree tiles, rounding up or down
-
-    Args:
-        swath_length_width (iterable[float, float]): sizes in degrees of swath
+    Attributes:
+        sentinel_list (list[Sentinel]): Sentinel objects covering one area
         tile_size (float): default 0.5, degrees of tile size to aim for
             Note: This will be adjusted to make even tiles
         overlap (float): default 0.1, overlap size between adjacent blocks
-
-    Examples:
-    >>> print(tile_dims(1.3, tile_size=0.5, overlap=0.1))
-    0.5
-    >>> test_lengths = np.linspace(1, 2, 11)
-    >>> print(tile_dims(test_lengths))
-    [ 0.55        0.6         0.46666667  0.5         0.53333333  0.56666667
-      0.475       0.5         0.525       0.55        0.48      ]
-
     """
-    swath_lw_arr = np.array(swath_length_width)
-    num_tiles_arr = num_tiles(swath_lw_arr, tile_size, overlap)
 
-    # how much length is covered if we spread out overlap
-    size_unoverlapped = swath_lw_arr + (num_tiles_arr - 1) * overlap
-    # Use this to get each tile's size
-    return size_unoverlapped / num_tiles_arr
+    def __init__(self, sentinel_list, tile_size=0.5, overlap=0.1):
+        self.sentinel_list = sentinel_list
+        self.tile_size = tile_size
+        self.overlap = overlap
 
+    @property
+    def extent(self):
+        return self.total_swath_extent()
 
-def make_tiles(extent, tile_size=0.5, overlap=0.1):
-    """Given a swath extent, divide into overlapping tiles
+    def total_swath_extent(self):
+        """Take a list of Sentinels, and find total extent of area covered
 
-    Args:
-        extent (tuple[float, float, float, float]): Sentinel.swath_extent
-            (lon_left,lon_right,lat_bottom,lat_top)
-        tile_size (float): default 0.5, degrees of tile size to aim for
-            Note: This will be adjusted to make even tiles
-        overlap (float): default 0.1, overlap size between adjacent blocks
-    Returns:
-        list[tuple[float]]: list of tiles in order
-    """
-    min_lon, max_lon, min_lat, max_lat = extent
-    height_width_arr = np.array([max_lon - min_lon, max_lat - min_lat])
+        Returns:
+            tuple[float]: (min_lon, max_lon, min_lat, max_lat)
+        """
+        swath_extents = np.array([s.swath_extent for s in self.sentinel_list])
+        min_lon, _, min_lat, _ = np.min(swath_extents, axis=0)
+        _, max_lon, _, max_lat = np.max(swath_extents, axis=0)
+        return min_lon, max_lon, min_lat, max_lat
 
-    num_lat_tiles, num_lon_tiles = num_tiles(height_width_arr, tile_size=tile_size, overlap=overlap)
-    print("Num lat tiles: %s" % num_lat_tiles)
-    print("Num lon tiles: %s" % num_lon_tiles)
-    lat_tile_size, lon_tile_size = tile_dims(height_width_arr, tile_size=tile_size, overlap=overlap)
+    @property
+    def total_width_height(self):
+        """Width and height in deg across all swath areas in sentinel_list
 
-    tiles = []
-    cur_lat, cur_lon = min_lat, min_lon
-    # Iterate bot to top, left to right
-    for latidx in range(num_lat_tiles):
-        for lonidx in range(num_lon_tiles):
-            t = Tile(cur_lat, cur_lon, height=lat_tile_size, width=lon_tile_size)
-            tiles.append(t)
-            cur_lon = cur_lon + lon_tile_size - overlap
-        cur_lon = min_lon  # Reset after each left-to-right
-        cur_lat = cur_lat + lat_tile_size - overlap
+        Width is longitude extent, height is latitude extent
 
-    return tiles
+        Returns:
+            ndarray[float, float]: (size_lon, size_lat) in degrees
+        """
+        left, right, bot, top = self.extent
+        return np.array([right - left, top - bot])
+
+    @staticmethod
+    def calc_num_tiles(length, tile_size, overlap):
+        """Given an edge length (of a swath), find number of tiles in a direction
+
+        Illustration: With tile_size=0.5 and overlap=0.1,
+        dividing 1.3 total length into 3 overlap blocks:
+
+        |     | |   | |    |
+        -----------------------
+          .4  .1 .3 .1  .4
+
+        Examples:
+        >>> Sentinel.num_tiles(1.3, tile_size=0.5, overlap=0.1)
+        3
+        >>> Sentinel.num_tiles(np.array([1.3, 1.3]), tile_size=0.5, overlap=0.1)
+        array([3, 3])
+        >>> Sentinel.num_tiles(np.array([1.4, 1.5, 1.6]), tile_size=0.5, overlap=0.1)
+        array([3, 3, 4])
+        """
+        covered_width = tile_size - overlap
+        length_to_divide = length - overlap
+        return np.round((length_to_divide) / covered_width).astype(int)
+
+    @property
+    def num_tiles(self):
+        return self.calc_num_tiles(self.total_width_height, self.tile_size, self.overlap)
+
+    @staticmethod
+    def calc_tile_dims(length_arr, tile_size, overlap):
+        """Given a swath (size_lon, width), find sizes of tiles
+
+        If tile_size = 0.5 and overlap = 0.1, the function will
+        try to divide into about 0.5 degree tiles, rounding up or down
+
+        Args:
+            length_arr (iterable[float, float]): sizes in degrees of swath
+            tile_size (float): degrees of tile size to aim for
+                Note: This will be adjusted to make even tiles
+            overlap (float): overlap size between adjacent blocks
+
+        Examples:
+        >>> print(Sentinel.calc_tile_dims(1.3, 0.5, 0.1))
+        0.5
+        >>> test_lengths = np.linspace(1, 2, 11)
+        >>> print(Sentinel.calc_tile_dims(test_lengths, 0.5, 0.1))
+        [ 0.55        0.6         0.46666667  0.5         0.53333333  0.56666667
+          0.475       0.5         0.525       0.55        0.48      ]
+        """
+        num_tiles_arr = TileGrid.num_tiles(length_arr, tile_size, overlap)
+        # how much length is covered if we spread out overlap
+        size_unoverlapped = length_arr + (num_tiles_arr - 1) * overlap
+        # Use this to get each tile's size
+        return size_unoverlapped / num_tiles_arr
+
+    @property
+    def tile_dims(self):
+        return self.calc_tile_dims(self.total_width_height, self.tile_size, self.overlap)
+
+    def make_tiles(self):
+        """Divide the extent from the sentinel_list into Tiles
+
+        Args:
+            extent (tuple[float, float, float, float]): Sentinel.swath_extent
+                (lon_left,lon_right,lat_bottom,lat_top)
+
+        Returns:
+            list[Tile]: list of tiles in ordered bot to top, left to right
+        """
+        tiles_out = []
+
+        min_lon, _, min_lat, _ = self.extent
+        cur_lat, cur_lon = min_lat, min_lon
+
+        # Iterate bot to top, left to right
+        num_lat_tiles, num_lon_tiles = self.num_tiles
+        lon_tile_size, lat_tile_size = self.tile_dims
+        for latidx in range(num_lat_tiles):
+            for lonidx in range(num_lon_tiles):
+                t = Tile(cur_lat, cur_lon, height=lat_tile_size, width=lon_tile_size)
+                tiles_out.append(t)
+                cur_lon = cur_lon + lon_tile_size - self.overlap
+            cur_lon = min_lon  # Reset after each left-to-right
+            cur_lat = cur_lat + lat_tile_size - self.overlap
+
+        return tiles_out
