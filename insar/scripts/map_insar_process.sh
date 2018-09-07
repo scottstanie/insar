bafbf65c6cdb4be95cc638ec9aa107f868b115aa
scott <scott.stanie@gmail.com>
2018-09-07 18:50:29 -0500

    add script to run insar process over tiles

---
+++ insar/scripts/map_insar_process.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+SCRIPTNAME=`basename "$0"`
+
+if [ "$#" -lt 1 ]
+then
+	echo 'Usage: $SCRIPTNAME "--args for --insar-process'
+	echo 'Make sure to double quote arg string'
+	exit 1
+fi
+COMMAND=$1
+echo "Running command over each directory in current folder:"
+FULL_CMD="insar process $COMMAND"
+echo $FULL_CMD
+echo "Will append --geojson with current directory's file"
+
+geojson_file=$(eval "ls *.geojson | head -1")
+	CUR_CMD="$FULL_CMD --geojson $geojson_file"
+	echo $CUR_CMD
+	eval $CUR_CMD
+	exit 1
+for dirname in $(find -maxdepth 1 -type d ! -name "." | head -5); do
+	echo "Moving to $dirname"
+	geojson_file=$(eval "ls *.geojson | head -1")
+	CUR_CMD="$FULL_CMD --geojson $geojson_file"
+	echo $CUR_CMD
+	eval $CUR_CMD
+	echo "gj: $geojson_file"
+done
