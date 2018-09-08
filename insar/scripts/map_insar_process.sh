628bc11cc38bb7b7913e946dbef645f854bbb77a
scott <scott.stanie@gmail.com>
2018-09-07 19:10:42 -0500

    add the directory walking cd commands

--- insar/scripts/map_insar_process.sh
+++ insar/scripts/map_insar_process.sh
@@ -4,20 +4,25 @@ SCRIPTNAME=`basename "$0"`
 
 if [ "$#" -lt 1 ]
 then
-	echo 'Usage: $SCRIPTNAME "--args for --insar-process'
-	echo 'Make sure to double quote arg string'
+	echo 'Usage: $SCRIPTNAME "--args for --insar-process"'
+	echo "Runs over pre-unzipped directories"
+	echo "Make sure to double quote arg string"
 	exit 1
 fi
 COMMAND=$1
 echo "Running command over each directory in current folder:"
-FULL_CMD="insar process $COMMAND"
+FULL_CMD="insar process --no-unzip $COMMAND "
 echo $FULL_CMD
 echo "Will append --geojson with current directory's file"
 
 for dirname in $(find -maxdepth 1 -type d ! -name "." | head -5); do
 	echo "Moving to $dirname"
+	cd "$dirname"
 	geojson_file=$(eval "ls *.geojson | head -1")
 	CUR_CMD="$FULL_CMD --geojson $geojson_file"
 	echo $CUR_CMD
 	eval $CUR_CMD
+
+	echo "Done with $dirname, moving up 1 dir"
+	cd ..
 done
