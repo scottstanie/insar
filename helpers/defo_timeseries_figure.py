d5f6d55919ac141ec13f03c71558b690b10062cb
scott <scott.stanie@gmail.com>
2018-09-26 09:32:11 -0500

    make figure plotting work

--- helpers/defo_timeseries_figure.py
+++ helpers/defo_timeseries_figure.py
@@ -1,27 +1,17 @@
-# coding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+from insar import latlon
+
 defo = np.load('deformation.npy')
 defo_img = latlon.LatlonImage(data=np.mean(defo[-3:], axis=0), dem_rsc_file='dem.rsc')
 plt.imshow(defo_img)
-timeseries = defo[:, 146, 250]
-plt.plot(timeseries)
+timeline = defo[:, 146, 250]
 geolist = np.load('geolist.npy')
-geolsit
-geolsit
-geolist
-plt.plot(geolist, timeline, marker='o', linestyle='dashed', linewidth=1, markersize=4)
-timeline = timeseries
-plt.plot(geolist, timeline, marker='o', linestyle='dashed', linewidth=1, markersize=4)
-plt.plot(geolist, timeline, marker='o', linestyle='dashed', linewidth=2, markersize=4)
-plt.plot(geolist, timeline, marker='x', linestyle='dashed', linewidth=2, markersize=6)
 plt.plot(geolist, timeline, marker='o', linestyle='dashed', linewidth=2, markersize=6)
-row = 146, col=250
-row, col=146, 250
-lat, lon = img.rowcol_to_latlon(row, col)
+row, col = 146, 250
 lat, lon = defo_img.rowcol_to_latlon(row, col)
 legend = ['Lat {:.3f}, Lon {:.3f}'.format(lat, lon)]
-legend
 plt.xlabel('SAR Image Date')
 plt.ylabel('Centimeters of uplift')
 plt.title('Deformation timeseries')
-get_ipython().run_line_magic('pinfo', 'plt.savefig')
 plt.savefig('defo_timeseries.png', bbox_inches='tight', transparent=True, dpi=300)
--- helpers/more_figures.py
+++ helpers/more_figures.py
@@ -1,38 +1,90 @@
-# coding: utf-8
-defo = np.load('deformation.npy')
-defo_img = latlon.LatlonImage(data=np.mean(defo[-3:], axis=0), dem_rsc_file='dem.rsc')
-plotting.plot_image_shifted(defo_img, cmap='seismic', label='Centimeters', perform_shift=False)
-plotting.plot_image_shifted(defo_img, cmap='discrete_seismic', label='Centimeters', perform_shift=False)
-plotting.plot_image_shifted(defo_img, cmap='discrete_seismic5', label='Centimeters', perform_shift=False)
-plotting.plot_image_shifted(defo_img, cmap='discrete_seismic7', label='Centimeters', perform_shift=False)
-plt.savefig('defo_img_figure.png', bbox_inches='tight', transparent=True, dpi=300)
-plotting.plot_image_shifted(defo_img, cmap='seismic', label='Centimeters', perform_shift=False)
-plt.savefig('defo_img_figure_seismic.png', bbox_inches='tight', transparent=True, dpi=300)
-plt.imshow(defo_img, cmap='seismic')
-plt.imshow(defo_img, cmap='seismic', vmax=5, vmin=-5))
-plt.imshow(defo_img, cmap='seismic', vmax=5, vmin=-5)
-plt.imshow(defo_img, cmap='seismic', vmax=8, vmin=-8)
-plt.axis('off')
-plt.savefig('defo_img_noaxes_seismic.png', bbox_inches='tight', transparent=True, dpi=300)
-plt.imshow(defo_img, cmap='discrete_seismic7')
-plt.imshow(defo_img, cmap='discrete_seismic7', vmax=5, vmin=5)
-plt.imshow(defo_img, cmap='discrete_seismic7', vmax=5, vmin=5)
-plt.imshow(defo_img, cmap='discrete_seismic7', vmax=5, vmin=-5)
-plt.axis('off')
-plt.savefig('defo_img_noaxes_discrete_seismic.png', bbox_inches='tight', transparent=True, dpi=300)
+import numpy as np
+import matplotlib.pyplot as plt
+from insar import latlon, plotting
+import sys
+
+# defo = np.load('deformation.npy')
+# defo_img = latlon.LatlonImage(data=np.mean(defo[-3:], axis=0), dem_rsc_file='dem.rsc')
 defo2 = np.load('/data3/scott/pecos/path85stitch/igrams/deformation.npy')
-geolist2 = np.load('/data3/scott/pecos/path85stitch/igrams/geolist.npy')
-defo_img2 = latlon.LatlonImage(data=np.mean(defo[-3:], axis=0), dem_rsc_file='/data3/scott/pecos/path85stitch/igrams/dem.rsc')
-defo_img2 = latlon.LatlonImage(data=np.mean(defo2[-3:], axis=0), dem_rsc_file='/data3/scott/pecos/path85stitch/igrams/dem.rsc')
-plt.imshow(defo_img2)
-plotting.plot_image_shifted(defo_img2, cmap='discrete_seismic7', label='Centimeters', perform_shift=False)
-plt.imshow(defo_img2, cmap='discrete_seismic7', vmin=-15, vmax=5)
-plt.imshow(defo_img2, cmap='discrete_seismic7', vmin=-7, vmax=7)
-plt.colorbar()
-plotting.plot_image_shifted(defo_img2, cmap='discrete_seismic7', label='Centimeters', perform_shift=True)
-plotting.plot_image_shifted(defo_img2, cmap='discrete_seismic7', label='Centimeters', perform_shift=True)
-plotting.plot_image_shifted(defo_img2, cmap='discrete_seismic7', label='Centimeters', perform_shift=True)
-plt.savefig('defo_img_large_figure_discrete_seismic.png', bbox_inches='tight', transparent=True, dpi=300)
-plotting.plot_image_shifted(defo_img2, cmap='discrete_seismic7', label='Centimeters', perform_shift=True, colorbar=False)
-plt.axis('off')
-plt.savefig('defo_img_large_noaxes_discrete_seismic.png', bbox_inches='tight', transparent=True, dpi=300)
+defo_img2 = latlon.LatlonImage(
+    data=np.mean(defo2[-3:], axis=0), dem_rsc_file='/data3/scott/pecos/path85stitch/igrams/dem.rsc')
+
+# fig, ax = plotting.make_figure_noborder()
+# nrows, ncols = defo_img2.shape
+# plotting.set_aspect_image(fig, defo_img2, height=8)
+# ax.imshow(defo_img2, cmap='seismic', vmax=8, vmin=-8, aspect='auto')
+# fig.savefig('test.png', dpi=80, transparent=True)
+vmin = -5
+vmax = 3.2
+nrows, ncols = defo_img2.shape
+fig, ax = plt.subplots(1, 1)
+plotting.set_aspect_image(fig, defo_img2, height=10)
+plotting.plot_image_shifted(
+    defo_img2,
+    fig=fig,
+    cmap='discrete_seismic7',
+    label='Centimeters',
+    vmin=vmin,
+    vmax=vmax,
+    perform_shift=True)
+fig.savefig(
+    'defo_img_large_figure_discrete_seismic.png', bbox_inches='tight', transparent=True, dpi=300)
+
+sys.exit()
+
+# FIRST: smaller image
+nrows, ncols = defo_img.shape
+
+fig, ax = plt.subplots(1, 1)
+plotting.plot_image_shifted(
+    defo_img, fig=fig, cmap='discrete_seismic7', label='Centimeters', perform_shift=False)
+fig.savefig('defo_img_figure.png', bbox_inches='tight', transparent=True, dpi=300)
+
+fig, ax = plt.subplots(1, 1)
+plotting.set_aspect_image(fig, defo_img2, height=8)
+plotting.plot_image_shifted(
+    defo_img, fig=fig, cmap='seismic', label='Centimeters', perform_shift=False)
+fig.savefig('defo_img_figure_seismic.png', bbox_inches='tight', transparent=True, dpi=300)
+
+# NOTE: for noaxes image, remove bbox_inches = tight
+fig, ax = plotting.make_figure_noborder()
+plotting.set_aspect_image(fig, defo_img, height=8)
+ax.imshow(defo_img, cmap='seismic', vmax=8, vmin=-8, aspect='auto')
+fig.savefig('defo_img_noaxes_seismic.png', transparent=True, dpi=300)
+
+fig, ax = plotting.make_figure_noborder()
+plotting.set_aspect_image(fig, defo_img, height=8)
+ax.imshow(defo_img, cmap='discrete_seismic7', vmax=5, vmin=-5, aspect='auto')
+print('defo_img_noaxes_discrete_seismic.png')
+fig.savefig('defo_img_noaxes_discrete_seismic.png', transparent=True, dpi=300)
+
+# SECOND: large image
+vmin = -5
+vmax = 3.2
+nrows, ncols = defo_img2.shape
+fig, ax = plt.subplots(1, 1)
+plotting.set_aspect_image(fig, defo_img2, height=10)
+plotting.plot_image_shifted(
+    defo_img2,
+    fig=fig,
+    cmap='discrete_seismic7',
+    label='Centimeters',
+    vmin=vmin,
+    vmax=vmax,
+    perform_shift=True)
+fig.savefig(
+    'defo_img_large_figure_discrete_seismic.png', bbox_inches='tight', transparent=True, dpi=300)
+
+fig, ax = plotting.make_figure_noborder()
+plotting.set_aspect_image(fig, defo_img2, height=24)
+plotting.plot_image_shifted(
+    defo_img2,
+    fig=fig,
+    cmap='discrete_seismic7',
+    label='Centimeters',
+    vmin=vmin,
+    vmax=vmax,
+    perform_shift=True,
+    colorbar=False)
+fig.savefig('defo_img_large_noaxes_discrete_seismic.png', transparent=True, dpi=300)
+print('defo_img_large_noaxes_discrete_seismic.png')
