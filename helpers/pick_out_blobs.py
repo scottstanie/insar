55b2b0e5b8e3bd24eebfa0226e5d77655668f25d
scott <scott.stanie@gmail.com>
2018-11-14 15:10:01 -0600

    helper blob scatter scripts

---
+++ helpers/pick_out_blobs.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+from insar import blob, plotting, latlon
+os.chdir('/data3/scott/pecos/path85stitch-linear/igrams')
+blobs_real = np.load('blobs.npy')
+blobs = np.load('blobs_0.5_0.2.npy')
+
+image = latlon.load_deformation_img('.')
+
+real_blob_locs = []
+real_blob_locs.append((141, 288))
+real_blob_locs.append((140, 608))
+real_blob_locs.append((140, 628))
+real_blob_locs.append((120, 605))
+real_blob_locs.append((620, 925))
+real_blob_locs.append((1060, 922))
+real_blob_locs.append((1010, 798))
+real_blob_locs.append((970, 778))
+real_blob_locs.append((1015, 75))
+real_blob_locs.append((972, 5))
+real_blob_locs.append((1060, 15))
+real_blob_locs.append((980, 55))
+real_blob_locs.append((1225, 1315))
+real_blob_locs.append((1163, 1295))
+
+plotting.plot_image_shifted(image)
+real_blob_locs = np.array(real_blob_locs)
+np.save('real_blob_locs', real_blob_locs)
+for y, x in real_blob_locs:
+    plt.plot(x, y, 'gx', ms=10)
+
+# btest = np.array([512, 558, 35, -2])
+truths = np.zeros(blobs.shape[0], ).astype(bool)
+for idx, b in enumerate(blobs):
+    m1 = blob.indexes_within_circle(mask_shape=image.shape, blob=b)
+    truths[idx] = any(m1[r, c] for r, c in real_blob_locs)
+
+blobs_bad = blobs[~truths]
+blobs_duped = blobs[truths]
+_, ax = blob.plot_blobs(image=image, blobs=blobs_bad)
+ax.set_title('blobs bad')
+_, ax = blob.plot_blobs(image=image, blobs=blobs_duped)
+ax.set_title('blobs duped')
