a7060bfb4f70eeb8bb78f17858925c0158e8a953
scott <scott.stanie@gmail.com>
2018-11-14 21:40:46 -0600

    blob tweaks

--- helpers/pick_out_blobs.py
+++ helpers/pick_out_blobs.py
@@ -3,16 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from insar import blob, plotting, latlon
-os.chdir('/data3/scott/pecos/path85stitch-linear/igrams')
-blobs_real = np.load('blobs.npy')
-blobs = np.load('blobs_0.5_0.2.npy')
-
-image = latlon.load_deformation_img('.')
-
 real_blob_locs = []
 real_blob_locs.append((141, 288))
 real_blob_locs.append((140, 608))
 real_blob_locs.append((140, 628))
+real_blob_locs.append((163, 618))
 real_blob_locs.append((120, 605))
 real_blob_locs.append((620, 925))
 real_blob_locs.append((1060, 922))
@@ -20,12 +15,21 @@ real_blob_locs.append((1010, 798))
 real_blob_locs.append((970, 778))
 real_blob_locs.append((1015, 75))
 real_blob_locs.append((972, 5))
+real_blob_locs.append((990, 110))
+real_blob_locs.append((934, 168))
 real_blob_locs.append((1060, 15))
 real_blob_locs.append((980, 55))
 real_blob_locs.append((1225, 1315))
+real_blob_locs.append((1225, 1315))
 real_blob_locs.append((1163, 1295))
 
+os.chdir('/data3/scott/pecos/path85stitch-linear/igrams')
+blobs_real = np.load('blobs.npy')
+blobs = np.load('blobs_0.5_0.2.npy')
+image = latlon.load_deformation_img('.')
+
 plotting.plot_image_shifted(image)
+
 real_blob_locs = np.array(real_blob_locs)
 np.save('real_blob_locs', real_blob_locs)
 for y, x in real_blob_locs:
--- helpers/plot_blob_scatters.py
+++ helpers/plot_blob_scatters.py
@@ -13,11 +13,19 @@ blobs_real_with_vars_ptp = blob.append_stats(blobs_real, image=image)
 blobs_with_vars_ptp = blob.append_stats(blobs_bad, image=image)
 blobs_all = np.vstack([blobs_real_with_vars_ptp, blobs_with_vars_ptp])
 clusters_3d = blob.cluster_blobs(np.abs(blobs_all))
+
 fig, ax = blob.scatter_blobs_3d(np.abs(blobs_with_vars_ptp), color='r', label='bad')
 blob.scatter_blobs_3d(np.abs(blobs_real_with_vars_ptp), color='g', label='real', ax=ax)
 fig.tight_layout()
 fig.legend()
 fig, axes = blob.scatter_blobs(np.abs(blobs_with_vars_ptp), color='r', label='bad')
 blob.scatter_blobs(np.abs(blobs_real_with_vars_ptp), color='g', label='real', axes=axes)
+
+truth = np.zeros(len(clusters_3d))
+truth[:len(blobs_real)] = 1
+print('Missed blobs:')
+print(blobs_all[np.where(truth - clusters_3d)[0]])
+f, a = plotting.plot_image_shifted(image)
+blob.plot_blobs(cur_axes=f.gca(), blobs=blobs_all[np.where(truth - clusters_3d)[0]])
 
 plt.show()
