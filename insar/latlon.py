a45b7660735e3fc97f41fd3da2b7a93ed8a48552
scott <scott.stanie@gmail.com>
2019-01-10 10:18:38 -0600

    float slicing working for LatlonImage

--- insar/latlon.py
+++ insar/latlon.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import copy
+from collections import Iterable
 from numpy import sin, cos, sqrt, arctan2, radians
 from xml.etree import ElementTree
 import os
@@ -11,27 +12,54 @@ logger = get_log()
 
 
 class LatlonImage(np.ndarray):
+    """A class wrapping numpy ndarray with lat/lon info from DEM .rsc file data
+
+    Has the ability to index values by [lat, lon] (as well as normal [row, col])
+        lat, lon = 31.456, -102.54
+        A[lat, lon]
+
+    Can also slice:
+        startlat, startlon = 31.456, -102.54
+        endlat, endlon = 31.0, -102.0
+        A[startlat:endlat, startlon:endlon]
+
+    Caveat: remember that high latitudes are the top of the array,
+    so if you want, e.g. N31.5 to N31.0, you need to say the higher latitude first.
+    Pretend you are looking at a map with lat/lon, and just reading
+    out the rows by their lats, columns by their lons
+
+
+    In addition to the ndarray attributes, there are:
+
+    Attributes:
+        filename (str): Name of file where data was loaded from (if from file)
+        dem_rsc_file (str): filename of the dem.rsc file with info (see below)
+        dem_rsc (dict): lat/lon and file infomation about image
+        dem_rsc_is_valid (bool): flag to indicate if the `dem_rsc` applies to image.
+            becomes invalid when, for example, 1D slicing occurs to get one row
+    Keys from the dem_rsc dict which are also attributes:
+        width: number of cols in image (redundant with ncols)
+        file_length: number of rows in image (redundant with nrows)
+        x_first: location of first column
+        y_first: location of first row
+        x_step: size between rows in `x_unit`
+        y_step: size between columns in `y_unit`
+        x_unit (degrees, almost always)
+        y_unit (degrees)
+        z_offset (0)
+        z_scale (1)
+        projection (LL)
+    """
+
     def __new__(cls, data=None, filename=None, dem_rsc_file=None, dem_rsc=None):
         """Can pass in either filenames to load, or 2D arrays/dem_rsc dicts
 
-        https://docs.scipy.org/doc/numpy/user/basics.subclassing.html
-        Custom attributes on top of np.array:
-            filename
-            dem_rsc_file
-            dem_rsc
-            dem_rsc_is_valid
-        Keys from the dem_rsc dict which are also attributes:
-            width
-            file_length
-            x_first
-            y_first
-            x_step
-            y_step
-            x_unit (degrees, almost always)
-            y_unit (degrees)
-            z_offset (0)
-            z_scale (1)
-            projection (LL)
+        if __new__() returns an instance of cls, then the new instance’s __init__()
+        is then called
+        Otherwise, the __init__ won't get called:
+        https://docs.scipy.org/doc/numpy/user/basics.subclassing.html#a-brief-python-primer-on-new-and-init
+
+        reference: https://docs.scipy.org/doc/numpy/user/basics.subclassing.html
         """
         # TODO: do we need to check that the rsc info matches the data?
         if data is None and filename is None:
@@ -76,6 +104,13 @@ class LatlonImage(np.ndarray):
         return obj
 
     def __array_finalize__(self, obj):
+        """Called when
+
+        Reference:
+        https://docs.scipy.org/doc/numpy/user/basics.subclassing.html#the-role-of-array-finalize
+
+        Deals with the view casting and new-from-template
+        """
         if obj is None:
             return
         self.filename = getattr(obj, 'filename', None)
@@ -88,6 +123,7 @@ class LatlonImage(np.ndarray):
         self.points = getattr(obj, 'points', None)
 
     def _disable_dem_rsc(self, sliced):
+        """Some slice occurred which makes image data no longer apply"""
         sliced.dem_rsc_is_valid = False
         return sliced
 
@@ -96,6 +132,10 @@ class LatlonImage(np.ndarray):
 
         Will get the right starts and steps to pass to `crop_rsc_data`
         """
+        # import ipdb
+        # ipdb.set_trace()
+        if contains_floats(items):
+            items = self._convert_float_slice(items)
         sliced = super(LatlonImage, self).__getitem__(items)
         # __getitem__ called multiple times: only do extra on first
         if not isinstance(sliced, LatlonImage):
@@ -106,8 +146,6 @@ class LatlonImage(np.ndarray):
 
         # print('here')
         # print(sliced, items)
-        # import pdb
-        # pdb.set_trace()
         if not sliced.dem_rsc_is_valid or sliced.ndim < 2 or sliced.ndim > 3:
             return self._disable_dem_rsc(sliced)
 
@@ -121,7 +159,7 @@ class LatlonImage(np.ndarray):
         if isinstance(items, slice):
             # This is a slice along rows, all cols
             return sliced
-        elif isinstance(items, tuple):
+        elif isinstance(items, tuple) and all(isinstance(s, slice) for s in items):
             row_slice, col_slice = items
             # try:
             # except (TypeError, ValueError):
@@ -131,7 +169,7 @@ class LatlonImage(np.ndarray):
             # raise ValueError("Can only do 2D slices on %s" % self.__class__.__name__)
         else:
             return self._disable_dem_rsc(sliced)
-        # Why would we need to check for Nones? the crop function seems to handle fine...
+        # do we need to check for Nones? the crop function seems to handle fine...
         # if row_slice == slice(None) or col_slice == slice(None):
         return self._handle_dem_slice(sliced, row_slice, col_slice)
 
@@ -144,12 +182,63 @@ class LatlonImage(np.ndarray):
             return sliced
         elif isinstance(items, tuple):
             # If we did something like A[:, :4, :4], crop the dem, still valid
-            if len(items) == 3:
+            if len(items) == 3 and all(isinstance(s, slice) for s in items[1:]):
                 _, row_slice, col_slice = items
                 return self._handle_dem_slice(sliced, row_slice, col_slice)
             else:
-                # Didn't pass 3 slices... unsure what this would be now
+                # Didn't pass 3 slices...  maybe a list of indexes
                 return self._disable_dem_rsc(sliced)
+
+    def _convert_float_slice(self, slice_items):
+        """Convert lat/lon (float) slicing to row, cols
+
+        Cases to handle:
+            1. 2D image: A[lat, lon]
+            2. 3D stack: A[:, lat, lon] (timeseries at pixel)
+            3. slices within the lat, lon:
+                A[lat, lon1:lon2]
+        """
+        # E.g. (slice(-101.1,100.0,.05), slice(30.0, 31.0, .05))
+        if _is_float(slice_items):
+            raise IndexError("Can't specify only 1 float for lat/lon indexing")
+        # elif isinstance(slice_items, Iterable):
+        if len(slice_items) == 3 and self.ndim == 3:
+            # Something like [:, -101.1, 30.1]
+            dslice = slice_items[0]
+            lat, lon = slice_items[1:]
+        elif len(slice_items) == 2 and self.ndim == 2:
+            # Something like (-101.1, 30.0:30.2:.05)
+            dslice = None
+            lat, lon = slice_items
+        else:
+            raise IndexError(
+                "Invalid lat/lon slices for size %s LatlonImage: %s" % (self.ndim, slice_items))
+
+        if isinstance(lat, slice):
+            # Use class step size if None given
+            lat_start = lat.start or self.first_lat
+            lat_stop = lat.stop or self.last_lat
+            lat_step = lat.step or self.lat_step
+            lat = np.arange(lat_start, lat_stop, lat_step)
+        print(lat)
+        if isinstance(lon, slice):
+            lon_start = lon.start or self.first_lon
+            lon_stop = lon.stop or self.last_lon
+            lon_step = lon.step or self.lon_step
+            lon = np.arange(lon_start, lon_stop, lon_step)
+
+        print("Slicing lat = %s" % lat)
+        print("Slicing lon = %s" % lon)
+        rows, cols = self.nearest_pixel(lon=lon, lat=lat)
+        print("Row: %s" % rows)
+        print("Col: %s" % cols)
+        # TODO: do I care about converting lat slices to index slices? for now assume continuous list
+        # if these are lists/arrays, we need them to be slices
+        if isinstance(rows, Iterable):
+            rows = slice(min(rows), max(rows) + 1)
+        if isinstance(cols, Iterable):
+            cols = slice(min(cols), max(cols) + 1)
+        return (dslice, rows, cols) if dslice is not None else (rows, cols)
 
     def _handle_dem_slice(self, sliced, row_slice, col_slice):
         # print('sliced out shape', sliced.shape)
@@ -265,6 +354,18 @@ class LatlonImage(np.ndarray):
             return self.x_first + self.x_step * (self.shape[1] - 1)
 
     @property
+    def first_lat(self):
+        """alias to y_first, The latitude of the first row of the image"""
+        if self.dem_rsc_is_valid:
+            return self.y_first
+
+    @property
+    def first_lon(self):
+        """alias to x_first, The longitude of the first column of the image"""
+        if self.dem_rsc_is_valid:
+            return self.x_first
+
+    @property
     def lat_step(self):
         """The latitude increment for each pixel"""
         if self.dem_rsc_is_valid:
@@ -279,18 +380,35 @@ class LatlonImage(np.ndarray):
     def nearest_pixel(self, lon=None, lat=None):
         """Find the nearest row, col to a given lat and/or lon
 
-        Returns (tuple[int, int]): If both given, a pixel (row, col) is returned
+        Args:
+            lon (ndarray[float]): single or array of lons
+            lat (ndarray[float]): single or array of lat
+
+        Returns:
+            tuple[int, int]: If both given, a pixel (row, col) is returned
+            If array passed for either lon or lat, array is returned
             Otherwise if only one, it is (None, col) or (row, None)
         """
+
+        def _check_bounds(idx_arr, bound):
+            int_idxs = idx_arr.round().astype(int)
+            bad_idxs = np.logical_or(int_idxs < 0, int_idxs >= bound)
+            if np.any(bad_idxs):
+                # Need to check for single numbers, shape ()
+                if int_idxs.shape:
+                    # Replaces locations of bad_idxs with none
+                    int_idxs = np.where(bad_idxs, None, int_idxs)
+                else:
+                    int_idxs = None
+            return int_idxs
+
         out_row_col = [None, None]
-        if lon:
-            col_idx = (lon - self.x_first) / self.x_step
-            if col_idx >= 0 and col_idx < self.ncols:
-                out_row_col[1] = int(round(col_idx))
-        if lat:
-            row_idx = (lat - self.y_first) / self.y_step
-            if row_idx >= 0 and row_idx < self.nrows:
-                out_row_col[0] = int(round(row_idx))
+        if lon is not None:
+            col_idx_arr = (np.array(lon) - self.x_first) / self.x_step
+            out_row_col[1] = _check_bounds(col_idx_arr, self.ncols)
+        if lat is not None:
+            row_idx_arr = (np.array(lat) - self.y_first) / self.y_step
+            out_row_col[0] = _check_bounds(row_idx_arr, self.nrows)
 
         return tuple(out_row_col)
 
@@ -673,8 +791,8 @@ def box_area(box):
 
 def _check_valid_box(box):
     left, right, bot, top = box
-    assert (left <= right) and (bot <=
-                                top), "Box %s must be in form (left, right, bot, top)" % str(box)
+    if (left > right) or (bot > top):
+        raise ValueError("Box %s must be in form (left, right, bot, top)" % str(box))
 
 
 def intersect_area(box1, box2):
@@ -807,3 +925,21 @@ def stitch_images(image_list):
     # out = np.zero((total_rows, total_cols))
     # img1 = image_list[0]
     # out[:rows, :cols] = img1
+
+
+def _is_float(a):
+    return isinstance(a, float)
+
+
+def contains_floats(slice_items):
+    """Checks possible slice item values for floats"""
+    if _is_float(slice_items):
+        return True
+    elif isinstance(slice_items, Iterable):
+        for item in slice_items:
+            if _is_float(item):
+                return True
+            elif isinstance(item, slice):
+                if _is_float(item.start) or _is_float(item.stop):
+                    return True
+    return False
