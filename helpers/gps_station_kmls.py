abe4054bea22a5c60c81d61adb31a15b6e858512
scott <scott.stanie@gmail.com>
2019-07-19 17:40:22 -0500

    add gps statation kml script

---
+++ helpers/gps_station_kmls.py
@@ -0,0 +1,23 @@
+import sys
+from apertools import kml, gps, sario
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Usage: python %s [rsc_filename]" % sys.argv[0], file=sys.stderr)
+        print("or: python %s [stackfile.h5]" % sys.argv[0], file=sys.stderr)
+        sys.exit(1)
+
+    if sys.argv[1].endswith('.h5'):
+        rsc_data = sario.load_dem_from_h5(h5file=sys.argv[1])
+        station_list = gps.stations_within_rsc(rsc_data=rsc_data)
+    else:
+        station_list = gps.stations_within_rsc(rsc_filename=sys.argv[1])
+
+    for name, lat, lon in station_list:
+        kml.create_kml(
+            lon_lat=(lon, lat),
+            title=name,
+            desc="GPS station %s" % name,
+            shape='point',
+            kml_out='%s.kml' % name,
+        )
