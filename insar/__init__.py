356cf2311b8e5366eb481fab192758ae009c349a
scott <scott.stanie@gmail.com>
2019-09-05 10:19:07 -0500

    filter h5py warning

--- insar/__init__.py
+++ insar/__init__.py
@@ -1,6 +1,8 @@
 import warnings
 warnings.filterwarnings("ignore", message="numpy.dtype size changed")
 warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore", category=FutureWarning)
 # from . import blob
 # from . import mask
 # from . import tile
