34b15863441349381eb036fafad3176fbdea4345
scott <scott.stanie@gmail.com>
2020-04-19 15:41:51 -0500

    add file of constants

---
+++ insar/constants.py
@@ -0,0 +1,26 @@
+DATE_FMT = "%Y%m%d"
+
+MASK_FILENAME = "masks.h5"
+INT_FILENAME = "int_stack.h5"
+UNW_FILENAME = "unw_stack.h5"
+CC_FILENAME = "cc_stack.h5"
+
+# dataset names for general 3D stacks
+STACK_DSET = "stack"
+STACK_MEAN_DSET = "stack_mean"
+STACK_FLAT_DSET = "stack_flat"
+STACK_FLAT_SHIFTED_DSET = "stack_flat_shifted"
+
+# Mask file datasets
+GEO_MASK_DSET = "geo"
+GEO_MASK_SUM_DSET = "geo_sum"
+IGRAM_MASK_DSET = "igram"
+IGRAM_MASK_SUM_DSET = "igram_sum"
+
+DEM_RSC_DSET = "dem_rsc"
+
+GEOLIST_DSET = "geo_dates"
+INTLIST_DSET = "int_dates"
+
+REFERENCE_ATTR = "reference"
+REFERENCE_STATION_ATTR = "reference_station"
