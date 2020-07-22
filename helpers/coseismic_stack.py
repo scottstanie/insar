7800a9fb6d3eb028f56da988ce9ef8450c1e309e
scott <scott.stanie@gmail.com>
2020-07-22 11:31:50 -0500

    fix coseismic

--- helpers/coseismic_stack.py
+++ helpers/coseismic_stack.py
@@ -1,10 +1,10 @@
 # coding: utf-8
-from datetime import datetime
+from datetime import date
 import numpy as np
 import apertools.sario as sario
 
 
-def stack_igrams(event_date=datetime.date(2020, 3, 26)):
+def stack_igrams(event_date=date(2020, 3, 26), outname=None, verbose=True):
 
     geolist, intlist = sario.load_geolist_intlist('.')
     insert_idx = np.searchsorted(geolist, event_date)
@@ -13,8 +13,19 @@ def stack_igrams(event_date=datetime.date(2020, 3, 26)):
     geo_subset = geolist[-(2 * num_igrams):]
     stack_igrams = list(zip(geo_subset[:num_igrams], geo_subset[num_igrams:]))
     stack_fnames = sario.intlist_to_filenames(stack_igrams, '.unw')
+    if verbose:
+        print("Using the following igrams in stack:")
+        for f in stack_fnames:
+            print(f)
+
     dts = [(pair[1] - pair[0]).days for pair in stack_igrams]
     stack = np.sum([sario.load(f) / dt for (f, dt) in zip(stack_fnames, dts)])
     stack = np.sum(np.stack([sario.load(f) / dt for (f, dt) in zip(stack_fnames, dts)], axis=0),
                    axis=0)
+
+    if outname:
+        import h5py
+        with h5py.File(outname, 'w') as f:
+            f['stackavg'] = stack
+        sario.save_dem_to_h5(outname, sario.load("dem.rsc"))
     return stack
