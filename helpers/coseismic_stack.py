1daa45ab97cd7fabb40a054fb2a6e0805e5fd509
scott <scott.stanie@gmail.com>
2020-07-29 17:37:03 -0500

    refactor coseismic for pre/post

--- helpers/coseismic_stack.py
+++ helpers/coseismic_stack.py
@@ -4,9 +4,11 @@ import numpy as np
 import apertools.sario as sario
 from insar.prepare import remove_ramp
 
+MENTONE_EQ_DATE = date(2020, 3, 26)
+
 
 def stack_igrams(
-    event_date=date(2020, 3, 26),
+    event_date=MENTONE_EQ_DATE,
     num_igrams=None,
     use_cm=True,
     rate=False,
@@ -22,7 +24,40 @@ def stack_igrams(
 
     gi_file = "geolist_ignore.txt" if ignore_geos else None
     geolist, intlist = sario.load_geolist_intlist('.', geolist_ignore_file=gi_file)
-    print(geolist)
+    # stack_igrams = select_igrams(geolist, intlist, event_date, num_igrams=num_igrams)
+    stack_igrams = select_pre_event(geolist, intlist, event_date)
+    # stack_igrams = select_post_event(geolist, intlist, event_date)
+
+    stack_fnames = sario.intlist_to_filenames(stack_igrams, '.unw')
+    if verbose:
+        print("Using the following igrams in stack:")
+        for f in stack_fnames:
+            print(f)
+
+    dts = [(pair[1] - pair[0]).days for pair in stack_igrams]
+
+    cur_phase_sum, cc_stack = create_stack(
+        stack_igrams,
+        stack_fnames,
+        dts,
+        rate=rate,
+        use_cm=use_cm,
+        ref=ref,
+        window=window,
+        cc_thresh=cc_thresh,
+        avg_cc_thresh=avg_cc_thresh,
+        sigma_filter=sigma_filter,
+    )
+
+    if outname:
+        import h5py
+        with h5py.File(outname, 'w') as f:
+            f['stackavg'] = cur_phase_sum
+        sario.save_dem_to_h5(outname, sario.load("dem.rsc"))
+    return cur_phase_sum, cc_stack
+
+
+def select_igrams(geolist, intlist, event_date, num_igrams=None):
 
     insert_idx = np.searchsorted(geolist, event_date)
     num_igrams = num_igrams or len(geolist) - insert_idx
@@ -32,13 +67,29 @@ def stack_igrams(
     geo_subset = geolist[insert_idx - num_igrams:insert_idx + num_igrams]
 
     stack_igrams = list(zip(geo_subset[:num_igrams], geo_subset[num_igrams:]))
-    stack_fnames = sario.intlist_to_filenames(stack_igrams, '.unw')
-    if verbose:
-        print("Using the following igrams in stack:")
-        for f in stack_fnames:
-            print(f)
+    return stack_igrams
 
-    dts = [(pair[1] - pair[0]).days for pair in stack_igrams]
+
+def select_pre_event(geolist, intlist, event_date):
+    return [ifg for ifg in intlist if (ifg[0] < event_date and ifg[1] < event_date)]
+
+
+def select_post_event(geolist, intlist, event_date):
+    return [ifg for ifg in intlist if (ifg[0] > event_date and ifg[1] > event_date)]
+
+
+def create_stack(
+    stack_igrams,
+    stack_fnames,
+    dts,
+    rate=False,
+    use_cm=True,
+    ref=(5, 5),
+    window=5,
+    cc_thresh=None,
+    avg_cc_thresh=0.35,
+    sigma_filter=.3,
+):
     cur_phase_sum = np.zeros(sario.load(stack_fnames[0]).shape).astype(float)
     cc_stack = np.zeros_like(cur_phase_sum)
     # for pixels that get masked sometimes, lower that count in the final stack dividing
@@ -86,9 +137,4 @@ def stack_igrams(
         import insar.blob.utils as blob_utils
         cur_phase_sum = blob_utils.gaussian_filter_nan(cur_phase_sum, sigma_filter)
 
-    if outname:
-        import h5py
-        with h5py.File(outname, 'w') as f:
-            f['stackavg'] = cur_phase_sum
-        sario.save_dem_to_h5(outname, sario.load("dem.rsc"))
     return cur_phase_sum, cc_stack
