f3cb1cc30965c027fc5664be5b51627f184fb28c
scott <scott.stanie@gmail.com>
2021-09-09 18:40:19 -0500

    have setup ready

--- helpers/l1-l0-compare.py
+++ helpers/l1-l0-compare.py
@@ -1,6 +1,7 @@
-import os
+import sys
 from apertools import utils
 import subprocess
+
 
 """
 [scott@grace l1-l0-mountain-test-egm96]$ tree
@@ -12,12 +13,23 @@ import subprocess
 └── l1
     ├── data
 """
-for dirname, lvl in zip(["l1/data/", "l0/data/"], ["SLC", "RAW"]):
-    utils.mkdir_p(dirname)
+if len(sys.argv) > 1 and sys.argv[1].startswith("--down"):
+    for dirname, lvl in zip(["l1/data/", "l0/data/"], ["SLC", "RAW"]):
+        utils.mkdir_p(dirname)
+        with utils.chdir_then_revert(dirname):
+            cmd = f"asfdownload --proce {lvl} --start 2019-12-07 --end 2019-12-08 --dem ../../elevation.dem --rel 151"
+            print(cmd)
+            subprocess.run(cmd, shell=True)
+            cmd = f"asfdownload --proce {lvl} --start 2020-05-23 --end 2020-05-24 --dem ../../elevation.dem --rel 151"
+            print(cmd)
+            subprocess.run(cmd, shell=True)
+
+for dirname in ["l1/", "l0/"]:
     with utils.chdir_then_revert(dirname):
-        cmd = f"asfdownload --proce {lvl} --start 2020-05-23 --end 2020-05-24 --dem ../elevation.dem --rel 151"
-        print(cmd)
-        subprocess.run(cmd, shell=True)
-        cmd = f"asfdownload --proce {lvl} --start 2020-06-04 --end 2020-06-05 --dem ../elevation.dem --rel 151"
-        print(cmd)
-        subprocess.run(cmd, shell=True)
+        subprocess.run("ln -s data/* .", shell=True)
+
+
+for dirname in ["l1/", "l0/"]:
+    with utils.chdir_then_revert(dirname):
+        subprocess.run("eof", shell=True)
+        subprocess.run("insar process --step 3-9", shell=True)
