5189a1c613bbccfa5642a7136d1441a6d62a3ee4
scott <scott.stanie@gmail.com>
2022-03-30 09:53:08 -0500

    wrong email

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="insar",
     version="1.2.0",
     author="Scott Staniewicz",
-    author_email="scott.stanie@utexas.com",
+    author_email="scott.stanie@gmail.com",
     description="Tools for gathering and preprocessing InSAR data",
     long_description=long_description,
     long_description_content_type="text/markdown",
